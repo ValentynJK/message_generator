dbb9d5c5c74890dab737483c3bc02b91dc4a26b8
Valentyn <valentynejk@gmail.com>
2021-08-10 12:01:58 +0300

    Here we have random bricks for our message

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-const randome_number = num => {
+const random_number = num => {
     return Math.floor(Math.random()*num)
+}
+const message_elements = {
+    secret_level: ['top secret', 'firs level of secret', 'Not so secret', 'Just do not tell your mother'],
+    agent_name: ['Ithan Khan', 'Napoleon Solo', 'James Bond', 'Illya Kuryakin', 'Gary Unwin'],
+    agent_code: ['Dirty Jack', '002', '007', 'Friday', 'Gustav'], 
+    kind_of_mission: ['"you are about to die"', 'dreadful', 'child play', 'weekend', 'global scale'], 
+    enemie: ['russian KGB', 'North Korea', 'Belarus'], 
+    enemie_role: ['spy', 'agent', 'morons'], 
+    what_is_done: ['kidnapped', 'killed', 'stolen'], 
+    object_of_action: ['Royal Prince', 'Nuclear Engineer', 'Our happiness'], 
+    aim: ['restore the world force balance', 'kill them all', 'bring everything back'], 
+    tools: ['license to kill', 'new badass Astonmartin', 'M-16 on your back'], 
+    end_of_the_message: ['God save the Queen', 'Good bless you', 'Good luck', 'We will take care of your family']
+}
+let random_array = [];
+for (let element in message_elements) {
+    let rand_num = random_number(message_elements[element].length); 
+    random_array.push(message_elements[element][rand_num]);
 }
